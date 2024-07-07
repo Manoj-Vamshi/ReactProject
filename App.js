1d8e8c02b70455d51ae04095b68206b99fb7f341
Reva1402 <148901721+Reva1402@users.noreply.github.com>
2024-07-07 15:39:54 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
 import WelcomePage from './WelcomePage';
 import LoginPage from './LoginPage';
 import HomePage from './HomePage';
@@ -8,44 +8,51 @@ import PrivacyPolicy from './PrivacyPolicy';
 import TermsConditions from './TermsConditions';
 import ContactUs from './ContactUs';
 import PixVibe from './PixVibe';
-
+import SignupPage from './SignupPage';
+import Profile from './Profile';
 import './App.css';
 
-
-function App() {
+const App = () => {
   return (
     <Router>
-      <div className="App">
-        {/* <header>
-          <div className="header-links">
-            <a href="#">PixVibe</a>
-            <a href="#">Feed</a>
-            <a href="friends" >Friends</a>
-            <a href="#" navigate="/help">Help</a>
-          </div>
-          
-        </header> */}
-        <Routes>
-          <Route path="/" element={<WelcomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/homepage" element={<HomePage />} />
-          <Route path="/friends" element={<Friends />} />
-          <Route path="/privacypolicy" element={<PrivacyPolicy />} />
-          <Route path="/termsandconditions" element={<TermsConditions />} />
-          <Route path="/contactus" element={<ContactUs />} />
-          <Route path="/pixvibe" element={<PixVibe />} />
-        </Routes>
-        <footer>
-          <div className="footer-links">
-            <a href="pixvibe" >PixVibe @ 2024</a>
-            <a href="contactus" >Contact Us</a>
-            <a href="privacypolicy">Privacy Policy</a>
-            <a href="termsandconditions">Terms & Conditions</a>
-          </div>
-        </footer>
-      </div>
+
+        <div className="App app-background">
+          {/* <header>
+            <div className="header-links">
+              <Link to="/">PixVibe</Link>
+              <Link to="/homepage">Feed</Link>
+              <Link to="/friends">Friends</Link>
+              <Link to="/help">Help</Link>
+            </div>
+          </header> */}
+
+          <Routes>
+            <Route path="/" element={<WelcomePage />} />
+            <Route path="/signup" element={<SignupPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/homepage" element={<HomePage />}  />
+            <Route path="/friends" element={<Friends />} />
+            <Route path="/privacypolicy" element={<PrivacyPolicy />} />
+            <Route path="/termsandconditions" element={<TermsConditions />} />
+            <Route path="/contactus" element={<ContactUs />} />
+            <Route path="/pixvibe" element={<PixVibe />} />
+            <Route path="/profile/:friendId" element={<Profile />}  />
+          </Routes>
+
+          {/* <footer>
+            <div className="footer-links">
+              <Link to="/pixvibe">PixVibe @ 2024</Link>
+              <Link to="/contactus">Contact Us</Link>
+              <Link to="/privacypolicy">Privacy Policy</Link>
+              <Link to="/termsandconditions">Terms & Conditions</Link>
+            </div>
+          </footer> */}
+        </div>
+     
     </Router>
   );
 }
+
+
 
 export default App;
